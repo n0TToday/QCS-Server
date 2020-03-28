4b581f02b231a87d0b1af6de93d17f1519736ff5
n0T <nottodaylxw@gmail.com>
2020-03-28 19:43:33 +0800

    用户登录注册等逻辑的后端实现

---
+++ QCSWebServer/src/UserServlet.java
@@ -0,0 +1,128 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		PrintWriter printWriter = response.getWriter();
+		String method = request.getParameter("method");
+		String userid;
+		String username;
+		String userpwd;
+		String stats;
+		String newpwd;
+		String registdate;
+
+		switch (method) {
+//            注册
+		case "regist":
+			userid = request.getParameter("userid");
+			username = request.getParameter("username");
+			userpwd = request.getParameter("userpwd");
+			registdate = request.getParameter("registdate");
+			if (User.login(username) == "false") {
+				if (User.regist(userid, username, userpwd, registdate) == "true") {
+					System.out.println("注册成功");
+					printWriter.write("true");
+				} else {
+					System.out.println("注册失败");
+					printWriter.write("false");
+				}
+			} else {
+				System.out.println("用户名已被注册");
+				printWriter.write("usernamefalse");
+			}
+
+			break;
+
+//            登录
+		case "login":
+			username = request.getParameter("username");
+			userpwd = request.getParameter("userpwd");
+			userid = User.login(username);
+			if (userid != "false") {
+				stats = User.selectpwd(userid, userpwd);
+				if (stats != "false") {
+					printWriter.write(userid);
+				} else {
+					printWriter.write("pwdfalse");
+				}
+			} else {
+				printWriter.write("usernamefalse");
+			}
+			break;
+
+//            查找用户名
+		case "getusername":
+			userid = request.getParameter("userid");
+			username = User.getusername(userid);
+			if (username != "false") {
+				printWriter.write(username);
+			} else {
+				printWriter.write("false");
+			}
+			break;
+
+//            检查用户名重复
+		case "checkusername":
+			username = request.getParameter("username");
+			stats = User.login(username);
+			if (stats != "false") {
+				printWriter.write("false");
+			} else {
+				printWriter.write("true");
+			}
+			break;
+
+//            修改密码
+		case "updatepwd":
+			userid = request.getParameter("userid");
+			userpwd = request.getParameter("oldpwd");
+			newpwd = request.getParameter("newpwd");
+			stats = User.selectpwd(userid, userpwd);
+			if (stats != "false") {
+				stats = User.updatepwd(userid, newpwd);
+				if (stats != "false") {
+					printWriter.write("true");
+				} else {
+					printWriter.write("updatepwdfalse");
+				}
+			} else {
+				printWriter.write("oldpwdfalse");
+			}
+			break;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
