d51c1408e881a596a4f533da5370057ad28c5a8f
n0T <nottodaylxw@gmail.com>
2020-04-07 11:56:30 +0800

    Log 的 Set&Get

---
+++ QCSWebServer/src/LoadLog.java
@@ -0,0 +1,47 @@
+
+public class LoadLog {
+	public String logdate;
+	public String loguserid;
+	public String logmethod;
+	public String logthing;
+	public String logdata;
+	public String logresult;
+	
+	public String getLogdate() {
+		return logdate;
+	}
+	public void setLogdate(String logdate) {
+		this.logdate = logdate;
+	}
+	public String getLoguserid() {
+		return loguserid;
+	}
+	public void setLoguserid(String loguserid) {
+		this.loguserid = loguserid;
+	}
+	public String getLogmethod() {
+		return logmethod;
+	}
+	public void setLogmethod(String logmethod) {
+		this.logmethod = logmethod;
+	}
+	public String getLogthing() {
+		return logthing;
+	}
+	public void setLogthing(String logthing) {
+		this.logthing = logthing;
+	}
+	public String getLogdata() {
+		return logdata;
+	}
+	public void setLogdata(String logdata) {
+		this.logdata = logdata;
+	}
+	public String getLogresult() {
+		return logresult;
+	}
+	public void setLogresult(String logresult) {
+		this.logresult = logresult;
+	}
+	
+}
